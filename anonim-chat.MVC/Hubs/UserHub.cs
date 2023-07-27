51a072eebdcdf2d0bde7fe9ec0019db4af241e37
jamol <jamol@LAPTOP-5TNOU2IH>
2023-07-28 02:25:36 +0500

    Add project files.

---
+++ anonim-chat.MVC/Hubs/UserHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace anonim_chat.MVC.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string User,string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", User, message);
+        }
+    }
+}
